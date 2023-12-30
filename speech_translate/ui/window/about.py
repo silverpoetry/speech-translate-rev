silverpoetry/speech-translate-rev

commit 70587bb91a2e9a2f34c4c1489bed0e3e294b4b69

refactor imports in effort to improve startup speed
- try to fix wont start from prebuilt binary #62
- remove some type hinting
- add back splash image (but this time in main window directly)

speech_translate/ui/window/main.py
@@ -1,22 +1,21 @@
 import os
+import subprocess
 import sys
 from platform import processor, release, system, version
 from signal import SIGINT, signal  # Import the signal module to handle Ctrl+C
 from threading import Thread
 from time import sleep, strftime, time
-from tkinter import Frame, Menu, StringVar, Tk, Toplevel, filedialog, ttk
+from tkinter import Canvas, Frame, Menu, StringVar, Tk, Toplevel, filedialog, ttk
 from typing import Callable, Dict, Literal, Optional
 
 import pystray
 from loguru import logger
-from PIL import Image, ImageDraw
-from stable_whisper import WhisperResult
+from PIL import Image, ImageDraw, ImageTk
 from tkhtmlview import HTMLText
-from torch import cuda
 
 from speech_translate._constants import APP_NAME
 from speech_translate._logging import init_logging
-from speech_translate._path import dir_export, dir_log, p_app_icon
+from speech_translate._path import dir_export, dir_log, p_app_icon, p_splash_image
 from speech_translate._version import __version__
 from speech_translate.linker import bc, sj
 from speech_translate.ui.custom.checkbutton import CustomCheckButton
@@ -30,11 +29,6 @@ from speech_translate.ui.custom.dialog import (
 )
 from speech_translate.ui.custom.message import mbox
 from speech_translate.ui.custom.tooltip import tk_tooltip, tk_tooltips
-from speech_translate.ui.window.about import AboutWindow
-from speech_translate.ui.window.log import LogWindow
-from speech_translate.ui.window.setting import SettingWindow
-from speech_translate.ui.window.transcribed import TcsWindow
-from speech_translate.ui.window.translated import TlsWindow
 from speech_translate.utils.audio.device import (
     get_default_host_api,
     get_default_input_device,
@@ -43,8 +37,6 @@ from speech_translate.utils.audio.device import (
     get_input_devices,
     get_output_devices,
 )
-from speech_translate.utils.audio.file import mod_result, process_file, translate_result
-from speech_translate.utils.audio.record import record_session
 from speech_translate.utils.helper import (
     bind_focus_recursively,
     emoji_img,
@@ -71,12 +63,22 @@ from speech_translate.utils.whisper.download import (
     verify_model_faster_whisper,
     verify_model_whisper,
 )
-from speech_translate.utils.whisper.helper import (
-    append_dot_en,
-    create_hallucination_filter,
-    model_keys,
-    save_output_stable_ts,
-)
+from speech_translate.utils.whisper.helper import append_dot_en, create_hallucination_filter, model_keys
+
+
+# monkey patch subprocess.run
+class NoConsolePopen(subprocess.Popen):
+    """
+    A custom Popen class that disables creation of a console window in Windows.
+    """
+    def __init__(self, args, **kwargs):
+        if system() == 'Windows' and 'startupinfo' not in kwargs:
+            kwargs['startupinfo'] = subprocess.STARTUPINFO()
+            kwargs['startupinfo'].dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        super().__init__(args, **kwargs)
+
+
+subprocess.Popen = NoConsolePopen
 
 
 # modify static_ffmpeg add_paths
@@ -226,10 +228,28 @@ class MainWindow:
         # UI
         bc.mw = self
         self.root = Tk()
-        self.root.title(APP_NAME)
+        self.root.title(APP_NAME + " - Booting up...")
         self.root.protocol("WM_DELETE_WINDOW", self.on_close)
         self.root.minsize(600, 300)
         self.root.wm_attributes("-topmost", False)  # Default False
+        self.root.lift()
+        self.root.attributes('-topmost', True)
+        self.root.after_idle(self.root.attributes, '-topmost', False)
+
+        # splash
+        try:
+            self.splash_img = Image.open(p_splash_image)
+            self.splash_img = self.splash_img.resize((640, 360))
+        except Exception:
+            self.splash_img = Image.new("RGB", (640, 360), "black")
+
+        self.root.geometry("640x350")
+        self.canvas_splash = Canvas(self.root, width=640, height=345, bg="black", highlightthickness=0)
+        self.canvas_splash.pack(side="top", fill="both", expand=True)
+
+        self.img_splash = ImageTk.PhotoImage(self.splash_img)
+        self.canvas_splash.create_image(0, 170, image=self.img_splash, anchor="w")
+        self.root.update()
 
         # Flags
         self.always_on_top: bool = False
@@ -268,6 +288,7 @@ class MainWindow:
         bc.mic_emoji = emoji_img(20, "🎤", dark)
         bc.speaker_emoji = emoji_img(20, "🔊", dark)
         bc.cuda = check_cuda_and_gpu()
+        logger.info(f"GPU: {get_gpu_info()} | CUDA: {bc.cuda}")
 
         # ------------------ Frames ------------------
         self.f1_toolbar = ttk.Frame(self.root)
@@ -632,10 +653,7 @@ class MainWindow:
 
         # ------------------ on Start ------------------
         bind_focus_recursively(self.root, self.root)
-        self.root.geometry(sj.cache["mw_size"])
-        self.root.lift()
-        self.root.attributes('-topmost', True)
-        self.root.after_idle(self.root.attributes, '-topmost', False)
+        self.canvas_splash.destroy()
         self.__on_init()
         # ------------------ Set Icon ------------------
         try:
@@ -654,6 +672,9 @@ class MainWindow:
 
         create_hallucination_filter("rec", return_if_exist=True)
         create_hallucination_filter("file", return_if_exist=True)
+
+        self.root.title(APP_NAME)
+        self.root.geometry(sj.cache["mw_size"])
 
         Thread(target=self.check_ffmpeg_start, daemon=True).start()
 
@@ -1377,24 +1398,21 @@ class MainWindow:
             with open(file_path, "w", encoding="utf-8") as f:
                 f.write(text)
         else:
-            if len(results) == 0:
-                res = results[0]
-                assert isinstance(res, WhisperResult), (
-                    "Error result should be a WhisperResult, this should not happened. " \
-                    "Please report this as bug at https://github.com/Dadangdut33/Speech-Translate/issues"
-                )
-                save_output_stable_ts(res, f_name, [f_ext.replace(".", "")], sj)
-            else:
-                for i, res in enumerate(results):
-                    assert isinstance(res, WhisperResult), (
-                        "Error result should be a WhisperResult, this should not happened. " \
-                        "Please report this as bug at https://github.com/Dadangdut33/Speech-Translate/issues"
-                    )
+            try:
+                # pylint: disable=import-outside-toplevel
+                from speech_translate.utils.whisper.save import save_output_stable_ts
+                if len(results) == 0:
+                    res = results[0]
+                    save_output_stable_ts(res, f_name, [f_ext.replace(".", "")], sj)
+                else:
+                    for i, res in enumerate(results):
+                        save_name = f"{f_name}/exported_{i}"  # folderize it
+                        logger.debug(f"Exporting {mode}d text to {save_name}")
 
-                    save_name = f"{f_name}/exported_{i}"  # folderize it
-                    logger.debug(f"Exporting {mode}d text to {save_name}")
-
-                    save_output_stable_ts(res, save_name, [f_ext.replace(".", "")], sj)
+                        save_output_stable_ts(res, save_name, [f_ext.replace(".", "")], sj)
+            except Exception as e:
+                logger.exception(e)
+                self.error_notif(str(e))
 
         # open folder
         open_folder(file_path)
@@ -1654,6 +1672,7 @@ class MainWindow:
 
         # Start thread
         try:
+            from speech_translate.utils.audio.record import record_session  # pylint: disable=import-outside-toplevel
             device = mic if not is_speaker else speaker
             rec_thread = Thread(
                 target=record_session,
@@ -1760,6 +1779,7 @@ class MainWindow:
 
             # Start thread
             try:
+                from speech_translate.utils.audio.file import process_file  # pylint: disable=import-outside-toplevel
                 f_import_thread = Thread(
                     target=process_file, args=(list(files), model_tc, source, target, tc, tl, tl_engine), daemon=True
                 )
@@ -1842,6 +1862,7 @@ class MainWindow:
 
             # Start thread
             try:
+                from speech_translate.utils.audio.file import mod_result  # pylint: disable=import-outside-toplevel
                 refine_thread = Thread(target=mod_result, args=(files, model_tc, "refinement"), daemon=True)
                 refine_thread.start()
 
@@ -1926,6 +1947,7 @@ class MainWindow:
 
             # Start thread
             try:
+                from speech_translate.utils.audio.file import mod_result  # pylint: disable=import-outside-toplevel
                 align_thread = Thread(target=mod_result, args=(files, model_tc, "alignment"), daemon=True)
                 align_thread.start()
 
@@ -2013,6 +2035,7 @@ class MainWindow:
 
             # Start thread
             try:
+                from speech_translate.utils.audio.file import translate_result  # pylint: disable=import-outside-toplevel
                 res_tl_thread = Thread(target=translate_result, args=(files, tl_engine, lang_target), daemon=True)
                 res_tl_thread.start()
 
@@ -2058,6 +2081,7 @@ class MainWindow:
 def get_gpu_info():
     result = ""
     try:
+        from torch import cuda  # pylint: disable=import-outside-toplevel
         gpu_count = cuda.device_count()
         if gpu_count == 0:
             result = "No GPU detected"
@@ -2075,6 +2099,7 @@ def get_gpu_info():
 def check_cuda_and_gpu():
     result = ""
     try:
+        from torch import cuda  # pylint: disable=import-outside-toplevel
         if not cuda.is_available():
             result = "CUDA is not available! Using CPU instead"
         else:
@@ -2091,9 +2116,8 @@ def check_cuda_and_gpu():
 def main(with_log_init=True):
     if with_log_init:
         init_logging(sj.cache["log_level"])
-    logger.info(f"App Version: {__version__}")
+    logger.info(f"App Version: {__version__} - TIME: {strftime('%Y-%m-%d %H:%M:%S')}")
     logger.info(f"OS: {system()} {release()} {version()} | CPU: {processor()}")
-    logger.info(f"GPU: {get_gpu_info()} | CUDA: {check_cuda_and_gpu()}")
     logger.debug(f"Sys args: {sys.argv}")
     logger.debug("Loading UI...")
     # check tray
@@ -2103,6 +2127,13 @@ def main(with_log_init=True):
         AppTray()  # Start tray app in the background
     # --- GUI ---
     main_ui = MainWindow()
+
+    # pylint: disable=import-outside-toplevel
+    from speech_translate.ui.window.about import AboutWindow
+    from speech_translate.ui.window.log import LogWindow
+    from speech_translate.ui.window.setting import SettingWindow
+    from speech_translate.ui.window.transcribed import TcsWindow
+    from speech_translate.ui.window.translated import TlsWindow
     TcsWindow(main_ui.root)
     TlsWindow(main_ui.root)
     SettingWindow(main_ui.root)

speech_translate/utils/helper.py
@@ -17,7 +17,6 @@ from webbrowser import open_new
 from loguru import logger
 from notifypy import Notify, exceptions
 from PIL import Image, ImageDraw, ImageFont, ImageTk
-from stable_whisper import WhisperResult
 
 from speech_translate._constants import APP_NAME, HACKY_SPACE
 from speech_translate._path import APP_ICON_MISSING, p_app_icon, p_font_emoji
@@ -148,11 +147,13 @@ def unique_rec_list(list_of_data: List):
     if len(list_of_data) == 0:
         return list_of_data
 
-    if isinstance(list_of_data[0], WhisperResult):
+    if isinstance(list_of_data[0], List):
+        # Convert the list to a set to get unique values then convert them back to a list
+        unique_lists = list(OrderedDict.fromkeys(list_of_data))
+    else:
         seen = set()
         unique_lists = []
         for obj in list_of_data:
-            assert isinstance(obj, WhisperResult)
             meta = ""
             try:
                 # get some metadata in first segment to make it more unique
@@ -166,9 +167,6 @@ def unique_rec_list(list_of_data: List):
             if check not in seen:
                 unique_lists.append(obj)
                 seen.add(check)
-    else:
-        # Convert the list to a set to get unique values then convert them back to a list
-        unique_lists = list(OrderedDict.fromkeys(list_of_data))
 
     return unique_lists
 

build.py
@@ -32,7 +32,12 @@ def clear_dir(_dir):
     try:
         if not os.path.exists(_dir):
             return
-        shutil.rmtree(_dir)
+        if os.path.isfile(_dir):
+            os.remove(_dir)
+        else:
+            # remove all files in the dir
+            for f_get in os.listdir(_dir):
+                os.remove(os.path.join(_dir, f_get))
     except Exception as e:
         print(f">> Failed to clear {_dir} reason: {e}")
 
@@ -45,11 +50,13 @@ def get_whisper_version():
 
 
 print(">> Clearing code folder")
-clear_dir("./speech_translate/_user")
+# clear_dir("./speech_translate/_user") # use this if base filter is updated
+clear_dir("./speech_translate/_user/settings.json")
 clear_dir("./speech_translate/export")
 clear_dir("./speech_translate/debug")
 clear_dir("./speech_translate/log")
 clear_dir("./speech_translate/temp")
+clear_dir("./speech_translate/assets/silero-vad/__pycache__")
 print(">> Done")
 print("Whisper version:", get_whisper_version())
 

speech_translate/__main__.py
@@ -1,41 +1,19 @@
-import os
-import subprocess
 import sys
-from platform import system
+from os import environ
 from warnings import simplefilter
-
-from numba.core.errors import NumbaDeprecationWarning, NumbaPendingDeprecationWarning
 
 from ._constants import LOG_FORMAT
 
 # override loguru default format so we dont need to do logger.remove on the logger init
-os.environ["LOGURU_FORMAT"] = LOG_FORMAT
+environ["LOGURU_FORMAT"] = LOG_FORMAT
 
 # If frozen, stdout will not work because there is no console. So we need to replace stdout
 # with stderr so that any module that uses stdout will not break the app
 if getattr(sys, "frozen", False):
     sys.stdout = sys.stderr
 
-
-# monkey patch subprocess.run
-class NoConsolePopen(subprocess.Popen):
-    """
-    A custom Popen class that disables creation of a console window in Windows.
-    """
-    def __init__(self, args, **kwargs):
-        if 'startupinfo' not in kwargs:
-            if system() == 'Windows':
-                kwargs['startupinfo'] = subprocess.STARTUPINFO()
-                kwargs['startupinfo'].dwFlags |= subprocess.STARTF_USESHOWWINDOW
-        super().__init__(args, **kwargs)
-
-
-subprocess.Popen = NoConsolePopen
-
-# remove numba warnings
-simplefilter("ignore", category=NumbaDeprecationWarning)
-simplefilter("ignore", category=NumbaPendingDeprecationWarning)
-simplefilter("ignore", category=UserWarning)  # supress general user warning like in pytorch
+# supress general user warning like in pytorch
+simplefilter("ignore", category=UserWarning)
 
 from .ui.window.main import main  # pylint: disable=wrong-import-position
 

speech_translate/_path.py
@@ -13,6 +13,7 @@ dir_refinement: str = os.path.abspath(os.path.join(dir_export, "@refined"))
 dir_translate: str = os.path.abspath(os.path.join(dir_export, "@translated"))
 dir_alignment: str = os.path.abspath(os.path.join(dir_export, "@aligned"))
 dir_silero_vad: str = os.path.abspath(os.path.join(dir_assets, "silero-vad"))
+p_app_settings: str = os.path.abspath(os.path.join(dir_user, "settings.json"))
 p_app_icon: str = os.path.abspath(os.path.join(dir_assets, "icon.ico"))
 p_font_emoji = os.path.abspath(os.path.join(dir_assets, "NotoEmoji-Bold.ttf"))
 p_splash_image: str = os.path.abspath(os.path.join(dir_assets, "splash.png"))

speech_translate/linker.py
@@ -1,20 +1,18 @@
-import copy
-import os
 from ast import literal_eval
+from copy import deepcopy
 from platform import system
 from shlex import quote
 from threading import Lock, Thread
 from tkinter import ttk
-from typing import TYPE_CHECKING, List, Literal, Optional, Sequence, Union
+from typing import TYPE_CHECKING, List, Literal, Optional, Union
 
-import tkhtmlview
 from PIL import ImageTk
-from stable_whisper import WhisperResult
+from tkhtmlview import HTMLText
 
 from speech_translate.utils.helper import generate_color, str_separator_to_html, wrap_result
 from speech_translate.utils.types import ToInsert
 
-from ._path import dir_debug, dir_export, dir_log, dir_temp, dir_user, p_app_icon
+from ._path import dir_debug, dir_export, dir_log, dir_temp, dir_user, p_app_icon, p_app_settings
 from .utils.setting import SettingJson
 
 if system() == "Windows":
@@ -37,9 +35,7 @@ if TYPE_CHECKING:
     from .ui.window.translated import TlsWindow
 
 # ------------------ #
-sj: SettingJson = SettingJson(
-    os.path.join(dir_user, "setting.json"), [dir_user, dir_temp, dir_log, dir_export, dir_debug], p_app_icon
-)
+sj: SettingJson = SettingJson(p_app_settings, [dir_user, dir_temp, dir_log, dir_export, dir_debug], p_app_icon)
 
 
 class BridgeClass:
@@ -95,8 +91,8 @@ class BridgeClass:
         self.current_rec_status: str = ""
         self.auto_detected_lang: str = "~"
         self.tc_lock: Optional[Lock] = None
-        self.tc_sentences: List[Union[WhisperResult, str]] = []
-        self.tl_sentences: List[Union[WhisperResult, str]] = []
+        self.tc_sentences: List = []
+        self.tl_sentences: List = []
 
         # file process
         self.file_tced_counter: int = 0
@@ -164,7 +160,7 @@ class BridgeClass:
         # we access setting using .get here to remove pylance warning "LiteralString" is not a string literal
         # the 0 for second argument is just a placeholder
         # make deepcopy because we would modify the list
-        copied_res = copy.deepcopy(res_with_conf)
+        copied_res = deepcopy(res_with_conf)
 
         # if not infinite and text too long
         # remove words from the start based on how over the limit it is
@@ -214,7 +210,7 @@ class BridgeClass:
                         {to_insert}
                     </div>"""
 
-        def update_it(widget: tkhtmlview.HTMLText, insert, pos):
+        def update_it(widget: HTMLText, insert, pos):
             if sj.cache.get(f"tb_{mode}_auto_scroll"):
                 widget.set_html(insert)
                 widget.see("end")
@@ -237,13 +233,13 @@ class BridgeClass:
             prev_pos = sb.get()[0]
             lbl.after(0, update_it, lbl, insert, prev_pos)
 
-    def map_result_lists(self, source_list: Sequence[Union[WhisperResult, str]], store_list: List[ToInsert], separator: str):
+    def map_result_lists(self, source_list, store_list: List[ToInsert], separator: str):
         """
         Map List of whisper result according to user setting while also calculating its color based on the confidence value.
 
         Parameters
         ----------
-        source_list : Sequence[Union[WhisperResult, str]]
+        source_list : 
             Source list to be mapped, can be either a list of whisper result or a list of string.
         store_list : List[ToInsert]
             List to store the mapped result.
@@ -326,12 +322,12 @@ class BridgeClass:
         self.update_tc(None, separator)
         self.update_tl(None, separator)
 
-    def update_tc(self, new_res: Union[WhisperResult, str, None], separator: str):
+    def update_tc(self, new_res, separator: str):
         """Update the transcribed text box with the new text.
 
         Parameters
         ----------
-        new_res : Union[WhisperResult, str]
+        new_res :
             New result to be added to the transcribed text box.
         separator : str
             Separator to be added to the end of the new result.
@@ -344,12 +340,12 @@ class BridgeClass:
         self.update_result_display(total_len, res_with_conf, "mw_tc")
         self.update_result_display(total_len, res_with_conf, "ex_tc")
 
-    def update_tl(self, new_res: Union[WhisperResult, str, None], separator: str):
+    def update_tl(self, new_res, separator: str):
         """Update the translated text box with the new text.
 
         Parameters
         ----------
-        new_res : Union[WhisperResult, str]
+        new_res : 
             New result to be added to the translated text box.
         separator :
             Separator to be added to the end of the new result.

speech_translate/ui/custom/dialog.py
@@ -3,7 +3,6 @@ from time import sleep
 from tkinter import BooleanVar, IntVar, StringVar, Text, Tk, Toplevel, filedialog, messagebox, ttk
 from typing import List, Literal, Union
 
-import stable_whisper
 from loguru import logger
 from tksheet import Sheet
 
@@ -736,7 +735,8 @@ class ModResultInputDialog:
         # if json and alignment try to get the language
         if self.mode == "Alignment" and self.mod_file and self.mod_file.endswith(".json"):
             try:
-                get = stable_whisper.WhisperResult(self.mod_file).language
+                from stable_whisper import WhisperResult  # pylint: disable=import-outside-toplevel
+                get = WhisperResult(self.mod_file).language
                 if get:
                     self.select_cb.current(WHISPER_LIST_UPPED.index(up_first_case(get)))
                     self.lang_value = up_first_case(get)

commit 40e159524cfa7e189deed6a1a203a7a4294bbbf7

add more root update to start
- to help prevent window freezes
- move create_hallucination_filter to thread
- fix unique_rec_list broken from previous commit 70587bb91a2e9a2f34c4c1489bed0e3e294b4b69

speech_translate/ui/window/about.py
@@ -140,10 +140,9 @@ class AboutWindow:
 
         self.checking = True
         self.update_text = "Checking..."
-        self.update_fg = "black"
+        self.update_fg = "yellow"
         self.tooltip_check_update.text = "Checking... Please wait"
         self.lbl_check_update.configure(text=self.update_text, foreground=self.update_fg)
-        self.root.update()
         logger.info("Checking for update...")
 
         Thread(target=self.req_update_check, daemon=True, args=[notify_up_to_date]).start()

speech_translate/ui/window/main.py
@@ -235,6 +235,10 @@ class MainWindow:
         self.root.lift()
         self.root.attributes('-topmost', True)
         self.root.after_idle(self.root.attributes, '-topmost', False)
+        try:
+            self.root.iconbitmap(p_app_icon)
+        except Exception:
+            pass
 
         # splash
         try:
@@ -244,11 +248,14 @@ class MainWindow:
             self.splash_img = Image.new("RGB", (640, 360), "black")
 
         self.root.geometry("640x350")
-        self.canvas_splash = Canvas(self.root, width=640, height=345, bg="black", highlightthickness=0)
+        self.canvas_splash = Canvas(self.root, width=640, height=325, bg="black", highlightthickness=0)
         self.canvas_splash.pack(side="top", fill="both", expand=True)
 
         self.img_splash = ImageTk.PhotoImage(self.splash_img)
-        self.canvas_splash.create_image(0, 170, image=self.img_splash, anchor="w")
+        self.canvas_splash.create_image(0, 160, image=self.img_splash, anchor="w")
+        self.lb_splash = ttk.Progressbar(self.root, orient="horizontal", length=640, mode="indeterminate")
+        self.lb_splash.pack(side="bottom", fill="x", expand=False)
+        self.lb_splash.start(15)
         self.root.update()
 
         # Flags
@@ -287,9 +294,13 @@ class MainWindow:
         bc.question_emoji = emoji_img(16, "❔", dark)
         bc.mic_emoji = emoji_img(20, "🎤", dark)
         bc.speaker_emoji = emoji_img(20, "🔊", dark)
-        bc.cuda = check_cuda_and_gpu()
-        logger.info(f"GPU: {get_gpu_info()} | CUDA: {bc.cuda}")
+        self.root.update()
 
+        def cuda_check():
+            bc.cuda = check_cuda_and_gpu()
+            logger.info(f"GPU: {get_gpu_info()} | CUDA: {bc.cuda}")
+
+        Thread(target=cuda_check, daemon=True).start()
         # ------------------ Frames ------------------
         self.f1_toolbar = ttk.Frame(self.root)
         self.f1_toolbar.pack(side="top", fill="x", expand=False, pady=(5, 0))
@@ -601,8 +612,9 @@ class MainWindow:
 
         # -- f4_statusbar
         # load bar
-        self.load_bar = ttk.Progressbar(self.f4_statusbar, orient="horizontal", length=100, mode="determinate")
+        self.load_bar = ttk.Progressbar(self.f4_statusbar, orient="horizontal", length=100, mode="indeterminate")
         self.load_bar.pack(side="left", padx=5, pady=5, fill="x", expand=True)
+        self.start_lb()
 
         # ------------------ Menubar ------------------
         self.menubar = Menu(self.root)
@@ -652,29 +664,32 @@ class MainWindow:
         self.root.bind("<F4>", self.open_detached_tlw)
 
         # ------------------ on Start ------------------
+        self.root.update()
         bind_focus_recursively(self.root, self.root)
-        self.canvas_splash.destroy()
         self.__on_init()
-        # ------------------ Set Icon ------------------
-        try:
-            self.root.iconbitmap(p_app_icon)
-        except Exception:
-            pass
 
     # on start
     def __on_init(self):
-        # update on start
+        # update widget on start
         self.cb_input_device_init()
+        self.root.update()
         self.cb_engine_change(sj.cache["tl_engine_mw"])
+        self.root.update()
         self.cbtn_task_change()
+        self.root.update()
 
         windows_os_only([self.radio_speaker, self.cb_speaker, self.lbl_speaker, self.btn_config_speaker])
+        self.root.update()
 
-        create_hallucination_filter("rec", return_if_exist=True)
-        create_hallucination_filter("file", return_if_exist=True)
+        Thread(target=create_hallucination_filter, args=["rec", True], daemon=True).start()
+        Thread(target=create_hallucination_filter, args=["file", True], daemon=True).start()
 
         self.root.title(APP_NAME)
         self.root.geometry(sj.cache["mw_size"])
+        self.canvas_splash.destroy()
+        self.lb_splash.destroy()
+        self.root.update()
+        self.stop_lb()
 
         Thread(target=self.check_ffmpeg_start, daemon=True).start()
 

speech_translate/utils/helper.py
@@ -147,7 +147,7 @@ def unique_rec_list(list_of_data: List):
     if len(list_of_data) == 0:
         return list_of_data
 
-    if isinstance(list_of_data[0], List):
+    if isinstance(list_of_data[0], str):
         # Convert the list to a set to get unique values then convert them back to a list
         unique_lists = list(OrderedDict.fromkeys(list_of_data))
     else: